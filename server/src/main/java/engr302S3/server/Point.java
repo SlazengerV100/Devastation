a7ae5c0fbcabb37e9b0474354ca8150eb0471c80
morleyjose <morleyjose@ecs.vuw.ac.nz>
2024-09-12 17:25:50 +1200

    Point, Tile, and TileType classes created from Nate's original design.

---
+++ server/src/main/java/engr302S3/server/Point.java
@@ -0,0 +1,6 @@
+package engr302S3.server;
+
+/**
+ * Coordinates of tiles in the devastation game
+ */
+public record Point(int x, int y){}
---
+++ server/src/main/java/engr302S3/server/TileType.java
@@ -0,0 +1,11 @@
+package engr302S3.server;
+
+/**
+ * Enum representing the possible types of content that a tile can hold.
+ */
+public enum TileType {
+  EMPTY,     // No content on the tile
+  STATION,   // Tile contains a Station
+  PLAYER,    // Tile contains a Player
+  TICKET     // Tile contains a Ticket
+}
