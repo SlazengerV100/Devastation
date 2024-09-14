4456b7e516e683b7e30a3a1d1a3150a4cc8e94ed
Kahu221 <128545581+Kahu221@users.noreply.github.com>
2024-09-14 17:41:17 +1200

    git push

--- client/src/stages/LoadingStage.jsx
+++ client/src/stages/LoadingStage.jsx
@@ -1,38 +1,33 @@
-import { useState } from 'react';
-import { connect, requestState } from '../managers/connectionManager.js'; // Adjust the import path as necessary
+import { connect } from '../managers/connectionManager.js'; // Adjust the import path as necessary
+import { useAtom } from "jotai";
+import {connectionStatusAtom} from '../js/atoms.js'
 
 const LoadingStage = () => {
-    const [gameState] = useState(null);
-    const [connectionStatus, setConnectionStatus] = useState('disconnected'); // can be 'disconnected', 'connecting', 'connected'
-    const [errorMessage, setErrorMessage] = useState(''); // For displaying errors
+    const [connectionStatus, setConnectionStatus] = useAtom(connectionStatusAtom)
 
-    // Connect to the WebSocket server
     const attemptConnect = async () => {
         setConnectionStatus('connecting');
-        setErrorMessage('');
         try {
             await connect();
             setConnectionStatus('connected');
         } catch (error) {
             console.error('Connection failed:', error);
             setConnectionStatus('disconnected');
-            setErrorMessage('Failed to connect: ' + error.message);
         }
     };
 
+    //TODO make this appealing this stage is minimal at best right now
     return (
         <div>
             {connectionStatus === 'connecting' && <p>Connecting...</p>}
             {connectionStatus === 'disconnected' && (
                 <div>
                     <p>Not connected</p>
-                    {errorMessage && <p>Error: {errorMessage}</p>}
                     <button onClick={attemptConnect}>Try Reconnecting</button>
 
                 </div>
             )}
-            {connectionStatus === 'connected' && <button onClick={requestState}>test</button>}
-            {gameState && <pre>{JSON.stringify(gameState, null, 2)}</pre>}
+            {connectionStatus === 'connected' && <p>Player has connected, swap to the game stage</p>}
         </div>
     );
 };
