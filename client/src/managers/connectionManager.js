d87d8a2ff4ca744bf7898af3bf4a9265ac388cba
burkitkahu <burkitkahu@myvuw.ac.nz>
2024-09-18 11:04:45 +1200

    pre rebase

--- client/src/managers/connectionManager.js
+++ client/src/managers/connectionManager.js
@@ -78,4 +78,12 @@ export const sendPlayerMovement = (name, direction) => {
     }
 };
 
+export const fetchActivePlayers = () => {
+    if(stompClient){
+        stompClient.send()
+    } else {
+        console.warn('Cannot send movement: not connected.');
+    }
+}
+
 
