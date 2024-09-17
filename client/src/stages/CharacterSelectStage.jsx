7ff0b69b3254624ef3b31829a89a10402acfcd53
Kahu221 <128545581+Kahu221@users.noreply.github.com>
2024-09-18 10:35:39 +1200

    idk

--- client/src/stages/CharacterSelectStage.jsx
+++ client/src/stages/CharacterSelectStage.jsx
@@ -1,6 +1,12 @@
-
+import {localCharacterAtom} from "../js/atoms.js";
+import {useEffect} from "react";
 
 const CharacterSelectStage = () => {
+
+    useEffect(() => {
+
+    }, [])
+
     return(
         <div>
             THIS IS A CHARACTER SELECT SCREEN
