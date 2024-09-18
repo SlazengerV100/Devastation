16959755272d5d328eaafb6722d28ff2ec895a8e
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2024-09-18 13:48:54 +1200

    Extend stations to get the relevant task that is being worked on and increment the time spent on it

--- server/src/main/java/engr302S3/server/Station.java
+++ server/src/main/java/engr302S3/server/Station.java
@@ -1,6 +1,10 @@
 package engr302S3.server;
 
+import engr302S3.server.ticketFactory.Task;
+import engr302S3.server.ticketFactory.Ticket;
 import lombok.Getter;
+
+import java.util.Optional;
 
 /**
  * Represents a work station in the game.
@@ -9,9 +13,11 @@ import lombok.Getter;
 public class Station {
     private final StationType stationType;
     private int progress;
+    private Optional<Ticket> ticketWorkingOn;
 
     /**
      * Create a work station with no task on it.
+     *
      * @param stationType the type of station.
      */
     public Station(StationType stationType) {
@@ -21,20 +27,33 @@ public class Station {
 
     /**
      * Whether the station is currently in use.
+     *
      * @return {@code true} if the station is in use, {@code false} otherwise.
      */
     public boolean inUse() {
-        return false;
+        return ticketWorkingOn.isPresent();
     }
 
     /**
      * Increment the progress indicator by one.
-     * @return {@code true} if the task is now complete as a result of the increment, {@code false} if the task is still in progress.
-     * @throws IllegalStateException if there is no task in progress on this station.
+     *
+     * @return {@code true} if the task is now complete as a result of the increment, {@code false} if the task is still in progress or there is no task to progress.
      */
     public boolean progress() {
+        if (ticketWorkingOn.isEmpty()) {
+            return false;
+        }
         progress++;
-        return false;
+        return getRelevantTask(ticketWorkingOn.get()).map(task -> progress >= task.getCompletionTime()).orElse(false);
+    }
+
+    /**
+     * Get the task that is relevant to this station
+     *
+     * @return the task that is associated with this station, or an {@link java.util.Optional#empty() Optional.empty()} if there is no task for this station.
+     */
+    private Optional<Task> getRelevantTask(Ticket ticket) {
+        return ticket.getTasks().stream().filter(task -> task.getType().equals(stationType)).findFirst();
     }
 
     /**
