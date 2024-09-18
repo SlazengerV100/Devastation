f836e48d2b39b06ab3515a5a18183418e531b436
morleyjose <morleyjose@ecs.vuw.ac.nz>
2024-09-18 18:25:30 +1200

    Finished the setup function of Devastation

--- server/src/main/java/engr302S3/server/Devastation.java
+++ server/src/main/java/engr302S3/server/Devastation.java
@@ -4,12 +4,9 @@ import engr302S3.server.players.Player;
 import engr302S3.server.players.Developer;
 import engr302S3.server.players.Tester;
 import engr302S3.server.players.ProjectManager;
-import engr302S3.server.ticketFactory.Ticket;
-import engr302S3.server.ticketFactory.TicketFactory;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -22,13 +19,11 @@ import java.util.List;
 @Component
 public class Devastation {
   private Tile[][] board; // A 2D array to represent the game board
-  private final int boardWidth = 20;         // Width of the game board
+  private final int boardWidth = 30;         // Width of the game board
   private final int boardHeight = 20;        // Height of the game board
   @Setter private int score;      // Game score
   @Setter private boolean running;        // Flag to indicate if the game is running
   private int timeLeft = 300;           // Time left for the game (seconds)
-  private TicketFactory ticketFactory;
-  private List<Ticket> activeTickets;
   private List<Player> players;
   private List<Station> stations;
 
@@ -57,32 +52,29 @@ public class Devastation {
         board[x][y] = new Tile(new Position(x, y)); // Create a tile at each point (x, y)
       }
     }
-
     initialiseObjects();
-
   }
 
   /**
    * initialise starting positions of game objects
    */
   public void initialiseObjects(){
-    this.activeTickets = new ArrayList<>();
-    this.ticketFactory = new TicketFactory();
     this.players = new ArrayList<>();
     this.stations = new ArrayList<>();
 
-    Player projectManager = new ProjectManager(new Position(1, 1));
-    Player developer = new Developer(new Position(2, 1));
-    Player tester = new Tester(new Position(3, 1));
-    // Add players
+    //create players and their starting positions
+    Player projectManager = new ProjectManager(new Position(2, 1));
+    Player developer = new Developer(new Position(3, 1));
+    Player tester = new Tester(new Position(4, 1));
+    // Add players to list
     this.players.add(projectManager);
     this.players.add(developer);
     this.players.add(tester);
 
     // Initialise player positions to tiles
-    board[1][1].setPlayer(projectManager);
-    board[2][1].setPlayer(developer);
-    board[3][1].setPlayer(tester);
+    board[2][1].setPlayer(projectManager);
+    board[3][1].setPlayer(developer);
+    board[4][1].setPlayer(tester);
 
     // Add Stations
     for (StationType type : StationType.values()) {
@@ -90,16 +82,13 @@ public class Devastation {
       this.stations.add(s);
     }
 
-    // Temporary station positions
-    board[9][4].setStation(stations.get(0));
-    board[9][10].setStation(stations.get(1));
-    board[9][16].setStation(stations.get(2));
-    board[16][4].setStation(stations.get(3));
-    board[16][10].setStation(stations.get(4));
-    board[16][16].setStation(stations.get(5));
-
-    // Generate one active ticket for testing and set tile to ticket
-    board[2][3].setTicket(generateTicket());
+    // Station positions
+    board[boardWidth/2][boardHeight/3].setStation(stations.get(0));
+    board[boardWidth/2][boardHeight/2].setStation(stations.get(1));
+    board[boardWidth/2][boardHeight*2/3].setStation(stations.get(2));
+    board[boardWidth*2/3][boardHeight/3].setStation(stations.get(3));
+    board[boardWidth*2/3][boardHeight/2].setStation(stations.get(4));
+    board[boardWidth*2/3][boardHeight*2/3].setStation(stations.get(5));
   }
 
   public void decreaseTime(){
@@ -118,15 +107,6 @@ public class Devastation {
       return board[x][y]; // Return the tile at the specified coordinates.
     }
     return null; // Return null if the coordinates are out of bounds.
-  }
-
-  /**
-   * Generate a ticket and add it to the active tickets
-   */
-  public Ticket generateTicket(){
-    Ticket ticket = ticketFactory.getTicket();
-    activeTickets.add(ticket);
-    return ticket;
   }
 
 }
