e2909d46d3d873c61facb149cd9f1ee73682f803
Kahu221 <128545581+Kahu221@users.noreply.github.com>
2024-09-24 00:49:32 +1200

    ANIMATED SPRITE WORKING, ref Game stage, game stage has been stripped to its bare and is ready to begin gameplay
    Added a pre load to preload all asset loading which is done by the init() function in sprite frame grabber

--- client/src/managers/StageManager.jsx
+++ client/src/managers/StageManager.jsx
@@ -1,17 +1,54 @@
 import { useEffect, useState } from 'react';
 import LoadingStage from '../stages/LoadingStage.jsx';
-import { useAtomValue, useAtom } from 'jotai';
-import { connectionStatusAtom, localCharacterAtom } from '../js/atoms.js';
 import CharacterSelectStage from "../stages/CharacterSelectStage.jsx";
 import GameStage from "../stages/GameStage.jsx";
 import { useAtom } from 'jotai';
 import { connectionStatusAtom, localCharacterAtom } from '../js/atoms.js';
 import { connect } from "./connectionManager.js";
+import {init} from "../js/spriteFrameGrabber.js";
 
 const StageManager = () => {
     const [connectionStatus, setConnectionStatus] = useAtom(connectionStatusAtom);
     const [storedPlayer, setStoredPlayer] = useAtom(localCharacterAtom);
-    const [currentStage, setCurrentStage] = useState(<LoadingStage />);
+
+    const [currentStage, setCurrentStage] = useState(<GameStage />);
+    const [loading, setLoading] = useState(true)
+    // Initial useEffect for asset initialization and connection attempt
+    useEffect(() => {
+        const initialize = async () => {
+            setLoading(true)
+            await init();
+            setLoading(false)
+            await attemptConnect();
+            console.log("Connection established!");
+        };
+
+        initialize();
+    }, []);
+
+    // Check for stored player in localStorage
+    useEffect(() => {
+        const checkStoredPlayer = () => {
+            const player = localStorage.getItem('playerID');
+            if (player) {
+                setStoredPlayer(prev => ({
+                    ...prev,
+                    playerName: player
+                }));
+            }
+        };
+        checkStoredPlayer();
+    }, [setStoredPlayer]);
+
+    useEffect(() => {
+        // if (connectionStatus === 'disconnected') {
+        //     setCurrentStage(<LoadingStage />);
+        // } else if (storedPlayer.playerName) {
+        setCurrentStage(<GameStage />);
+        //} else {
+        // setCurrentStage(<CharacterSelectStage />);
+        //}
+    }, [connectionStatus, storedPlayer.playerName]);
 
     const attemptConnect = async () => {
         try {
@@ -23,42 +60,14 @@ const StageManager = () => {
         }
     };
 
-    useEffect(() => {
-        attemptConnect().then(() => console.log("Connection established!"));
-    }, []);
-
-    // Check for stored player in localStorage
-    useEffect(() => {
-        const checkStoredPlayer = () => {
-            const player = localStorage.getItem('playerID');
-            if(player){
-                setStoredPlayer(prev => ({
-                    ...prev,
-                    playerName: player
-                }));
-            }
-        };
-
-        // Check initially when the component mounts
-        checkStoredPlayer();
-    }, [setStoredPlayer]);
-
-    // Trigger stage update based on connection status and storedPlayer changes
-    useEffect(() => {
-        console.log('CHANGED')
-        console.log(storedPlayer)
-        if (connectionStatus === 'disconnected') {
-            setCurrentStage(<LoadingStage />);
-        } else if (storedPlayer.playerName) {
-            setCurrentStage(<GameStage />);
-        } else {
-            setCurrentStage(<CharacterSelectStage />);
-        }
-    }, [connectionStatus, storedPlayer.playerName]); // Effect depends on connectionStatus and storedPlayer
-
     return (
        <>
-           {currentStage}
+           {
+               loading ?
+               <h1>Loading assets TODO fix this, its ugly</h1>
+               :
+               currentStage
+           }
        </>
     );
 };
--- client/src/stages/GameStage.jsx
+++ client/src/stages/GameStage.jsx
@@ -1,8 +1,9 @@
-import { Stage, Sprite } from '@pixi/react'; // Import Stage and Sprite from @pixi/react
+import {Stage, Sprite, AnimatedSprite} from '@pixi/react'; // Import Stage and Sprite from @pixi/react
 import { useAtomValue } from 'jotai';
 import { localCharacterAtom } from "../js/atoms.js";
 import map from '../../assets/map.png'; // Map image asset
 import useUpdatePosition from '../hooks/useUpdatePosition.js';
+import {textures} from '../js/spriteFrameGrabber.js'
 
 const GameStage = () => {
     const localCharacter = useAtomValue(localCharacterAtom);
@@ -19,6 +20,12 @@ const GameStage = () => {
                 x={window.innerWidth / 2}
                 y={window.innerHeight / 2}
                 anchor={0.5}
+
+            />
+            <AnimatedSprite
+                isPlaying={true}
+                textures={textures.running.DOWN}
+                animationSpeed={0.15}
             />
         </Stage>
     );
