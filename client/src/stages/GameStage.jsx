e4eb011c80d1591d7c2c3aa476a3b329b9b65ca7
Kahu221 <128545581+Kahu221@users.noreply.github.com>
2024-09-24 00:06:16 +1200

    map rendering fine

--- client/src/stages/GameStage.jsx
+++ client/src/stages/GameStage.jsx
@@ -1,40 +1,27 @@
-// GameStage.jsx
-import React from 'react';
-import useUpdatePosition from '../hooks/useUpdatePosition';
+import { Stage, Sprite } from '@pixi/react'; // Import Stage and Sprite from @pixi/react
 import { useAtomValue } from 'jotai';
-import {localCharacterAtom} from "../js/atoms.js";
+import { localCharacterAtom } from "../js/atoms.js";
+import map from '../../assets/map.png'; // Map image asset
+import useUpdatePosition from '../hooks/useUpdatePosition.js';
 
 const GameStage = () => {
+    const localCharacter = useAtomValue(localCharacterAtom);
     const updatePosition = useUpdatePosition();
-    const localCharacter = useAtomValue(localCharacterAtom)
 
     return (
-        <div>
-            this is a gameStage
-            <div>
-                <h2>{localCharacter.playerName} X: {localCharacter.x}, Y: {localCharacter.y}</h2>
-            </div>
-            <div>
-                <button onClick={() => {
-                    updatePosition("UP");
-                }}>UP
-                </button>
-                <button onClick={() => {
-                    updatePosition("DOWN");
-                }}>DOWN
-                </button>
-                <button onClick={() => {
-                    updatePosition("LEFT");
-                }}>LEFT
-                </button>
-                <button onClick={() => {
-                    updatePosition("RIGHT");
-                }}>RIGHT
-                </button>
-            </div>
-        </div>
+        <Stage
+            options={{ backgroundColor: 0x1099bb }}
+            width={window.innerWidth}
+            height={window.innerHeight}
+        >
+            <Sprite
+                image={map}
+                x={window.innerWidth / 2}
+                y={window.innerHeight / 2}
+                anchor={0.5}
+            />
+        </Stage>
     );
 };
 
 export default GameStage;
-
