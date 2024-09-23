595e8dc17207333836ff909603007b6424c9287b
nateburt <nateburt04@gmail.com>
2024-09-24 00:45:25 +1200

    Added update score method

--- server/src/main/java/engr302S3/server/Devastation.java
+++ server/src/main/java/engr302S3/server/Devastation.java
@@ -2,6 +2,8 @@ package engr302S3.server;
 
 import engr302S3.server.map.Board;
 
+import engr302S3.server.ticketFactory.Task;
+import engr302S3.server.ticketFactory.Ticket;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,4 +31,29 @@ public class Devastation {
     public void decreaseTime() {
         timeLeft --;
     }
+
+    /**
+     * Update the game score when a ticket is completed
+     * @param ticket completed ticket
+     */
+    public void updateScore(Ticket ticket){
+        int scorePerTask = 50;
+        int ticketScore = 0;
+        int maxScore = 1000;
+
+        // Update ticket score based on amount of tasks completed
+        for (Task task : ticket.getTasks()) {
+            if (task.getCompleted()) {
+                ticketScore += scorePerTask;
+            }
+        }
+
+        // Max ticket score can be 1000 and decreases to 0 based on time alive
+        ticketScore = Math.max(maxScore - (ticket.getTotalTime()*5), 0);
+        score += ticketScore;
+
+        ClientAPI.broadcastTicketResolve(ticket);
+        ClientAPI.broadcastScoreUpdate(score);
+    }
+
 }
