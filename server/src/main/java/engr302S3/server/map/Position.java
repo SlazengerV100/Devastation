f85b7e8b94581411f4dcd1d757c2589b745257cc
nateburt <nateburt04@gmail.com>
2024-09-30 17:07:35 +1300

    Revert "player indexing correctly"
    This reverts commit af811b846f8b4257c63dc36e0d70a4dc4a75c10e.

--- server/src/main/java/engr302S3/server/map/Position.java
+++ server/src/main/java/engr302S3/server/map/Position.java
@@ -8,8 +8,8 @@ package engr302S3.server.map;
 public record Position(int x, int y) {
 
     public Position {
-        //-1 for array index starting at 0
-        if (x < 0 || x > Board.BOARD_WIDTH - 1 || y < 0 || y > Board.BOARD_HEIGHT - 1 ) {
+
+        if (x < 0 || x > Board.BOARD_WIDTH || y < 0 || y > Board.BOARD_HEIGHT) {
             throw new IllegalArgumentException("Position x or y must be within range ( x = (0 ," + Board.BOARD_WIDTH + "), y = (0 ," + Board.BOARD_HEIGHT + ")");
         }
     }
